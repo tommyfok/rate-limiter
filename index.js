39d98ba671cc945a23a7c8169d96a53b4c1224e6
tommyfok <154274174@qq.com>
2020-11-17 14:35:35 +0800

    eslint fix

--- index.js
+++ index.js
@@ -1,94 +1,90 @@
-// read file test
-const fs = require('fs')
-const cluster = require('cluster')
-const uuidv4 = require('uuid/v4')
+const cluster = require('cluster');
+const uuidv4 = require('uuid/v4');
 
 // 默认每秒100次的限制
 class RateLimiter {
-    constructor(options = {}) {
-        if (options.limit > 10000) {
-            throw 'RateLimiter: limit too large, max value is 10000'
-        }
-
-        if (options.time > 24 * 3600 * 1000) {
-            throw 'RateLimiter: time too large, max value is 86400000'
-        }
-
-        let realOpts = Object.assign({
-            time: 1000,
-            limit: 1000
-        }, options)
-
-        this.time = realOpts.time
-        this.limit = realOpts.limit
-        this.key = realOpts.key
-
-        if (cluster.isMaster) {
-            this.cache = {}
-            cluster.on('message', (worker, message) => {
-                if (message.action === '_masterCheck') {
-                    let ctx = this
-                    worker.send({
-                        result: _masterCheck.call(ctx, message.key, message.now),
-                        checkId: message.checkId,
-                        action: '_checkDone'
-                    })
-                }
-            })
-        } else {
-            this.resolvers = {}
-            process.on('message', message => {
-                if (message.action === '_checkDone') {
-                    _finishCheck.call(this, message.checkId, message.result)
-                }
-            })
-        }
+  constructor(options = {}) {
+    if (options.limit > 10000) {
+      throw 'RateLimiter: limit too large, max value is 10000';
     }
 
-    check(key) {
-        key = key || this.key
-        if (cluster.isMaster) {
-            return new Promise(resolve => {
-                resolve(_masterCheck.call(this, key, Date.now()))
-            })
-        } else {
-            let checkId = uuidv4()
-            _callMasterCheck(key, checkId)
-            return new Promise(resolve => {
-                this.resolvers[checkId] = resolve
-            })
-        }
+    if (options.time > 24 * 3600 * 1000) {
+      throw 'RateLimiter: time too large, max value is 86400000';
     }
+
+    const realOpts = Object.assign({
+      time: 1000,
+      limit: 1000,
+    }, options);
+
+    this.time = realOpts.time;
+    this.limit = realOpts.limit;
+    this.key = realOpts.key;
+
+    if (cluster.isMaster) {
+      this.cache = {};
+      cluster.on('message', (worker, message) => {
+        if (message.action === '_masterCheck') {
+          const ctx = this;
+          worker.send({
+            result: _masterCheck.call(ctx, message.key, message.now),
+            checkId: message.checkId,
+            action: '_checkDone',
+          });
+        }
+      });
+    } else {
+      this.resolvers = {};
+      process.on('message', (message) => {
+        if (message.action === '_checkDone') {
+          _finishCheck.call(this, message.checkId, message.result);
+        }
+      });
+    }
+  }
+
+  check(key = this.key) {
+    if (cluster.isMaster) {
+      return new Promise((resolve) => {
+        resolve(_masterCheck.call(this, key, Date.now()));
+      });
+    }
+    const checkId = uuidv4();
+    _callMasterCheck(key, checkId);
+    return new Promise((resolve) => {
+      this.resolvers[checkId] = resolve;
+    });
+  }
 }
 
-module.exports = RateLimiter
+module.exports = RateLimiter;
 
 function _finishCheck(checkId, result) {
-    let resolver = this.resolvers[checkId]
-    if (resolver) {
-        resolver(result)
-    } else {
-        console.log('no resolver to finish check')
-    }
-    delete this.resolvers[checkId]
+  const resolver = this.resolvers[checkId];
+  if (resolver) {
+    resolver(result);
+  } else {
+    console.log('no resolver to finish check');
+  }
+  delete this.resolvers[checkId];
 }
 
 function _callMasterCheck(key, checkId) {
-    process.send({
-        key,
-        checkId,
-        now: Date.now(),
-        action: '_masterCheck'
-    })
+  process.send({
+    key,
+    checkId,
+    now: Date.now(),
+    action: '_masterCheck',
+  });
 }
 
 function _masterCheck(key, now) {
-    this.cache[key] = this.cache[key] || []
-    let list = this.cache[key].filter(ts => ts && (ts > (now - this.time)))
-    this.cache[key] = list
-    let allow = list.length < this.limit
-    if (allow) {
-        list.push(now)
-    }
-    return allow
+  this.cache[key] = this.cache[key] || [];
+  const list = this.cache[key].filter(ts => ts && (ts > (now - this.time)));
+  this.cache[key] = list;
+  const allow = list.length < this.limit;
+  if (allow) {
+    list.push(now);
+  }
+  return allow;
 }
--- test.js
+++ test.js
@@ -1,34 +1,34 @@
-const RL = require('./index')
+const RL = require('./index');
 const rl = new RL({
-    time: 100,
-    limit: 100
-})
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length
-const totalCheckCount = 80000
-const keyword = 'test'
+  time: 100,
+  limit: 100,
+});
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const totalCheckCount = 80000;
+const keyword = 'test';
 
 if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`)
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
-    }
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 }
 
 
-(async function() {
-    let now = Date.now()
-    let checkCount = Math.floor(totalCheckCount / (numCPUs + 1))
-    let allowCount = 0
+(async function () {
+  const now = Date.now();
+  const checkCount = Math.floor(totalCheckCount / (numCPUs + 1));
+  let allowCount = 0;
 
-    for (let i = 0; i < checkCount; i++) {
-        let checkTime = Date.now()
-        let allow = await rl.check(keyword)
-        if (allow === true) {
-            allowCount++
-        }
+  for (let i = 0; i < checkCount; i++) {
+    // const checkTime = Date.now();
+    const allow = await rl.check(keyword);
+    if (allow === true) {
+      allowCount += 1;
     }
+  }
 
-    let diffMs = Date.now() - now
-    console.log(`check keyword '${keyword}' ${checkCount} times, use ${(diffMs/checkCount).toFixed(4)}ms per check, ${diffMs}ms total, ${allowCount} allowed, this is ${cluster.isMaster?'master':'worker'}`)
-})()
+  const diffMs = Date.now() - now;
+  console.log(`check keyword '${keyword}' ${checkCount} times, use ${(diffMs / checkCount).toFixed(4)}ms per check, ${diffMs}ms total, ${allowCount} allowed, this is ${cluster.isMaster ? 'master' : 'worker'}`);
+}());
